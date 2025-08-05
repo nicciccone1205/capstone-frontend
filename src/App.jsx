5d44321ebb552b5ec83d86c3e044ff7eeccd84dc
Jamey Jones <jpkey764@gmail.com>
2025-08-04 20:35:08 -0400

    updated routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,17 @@ import { Route, Routes } from "react-router";
 import Layout from "./layout/Layout";
 import Login from "./auth/Login";
 import Register from "./auth/Register";
-import JeweleryPage from "./jewelery/JeweleryPage"
-import JeweleryOrder from "./jewelry/JeweleryOrder"
-import JeweleryPurchase from "./jewelery/JeweleryPurchase"
+import JewelryPage from "./jewelry/JewelryPage"
+import JewelryOrder from "./jewelry/JewelryOrders"
+import JewelryPurchase from "./jewelry/JewelryPurchase"
 
 export default function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
-      <Route path="/" element ={JeweleryPage/>}/>
-      <Route path="/order" element = {JeweleryOrder/>}/>
-      <Route path = "/purchase" element= {JeweleryPurchase/>}/>
+      <Route path="/" element ={<JewelryPage/>} />
+      <Route path="/order" element = {<JewelryOrder/>}/>
+      <Route path = "/purchase" element= {<JewelryPurchase/>}/>
         <Route index element={<p>Home page</p>} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
